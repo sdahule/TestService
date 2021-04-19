cf3961dd9993a5940b1fb88ba05557bbcaa293d8
swapnildahule <swapnildahule@localhost>
2021-04-19 22:09:19 +0530

    mongo db atlas db connection added

--- src/main/java/com/example/TestService/TestServiceApplication.java
+++ src/main/java/com/example/TestService/TestServiceApplication.java
@@ -1,13 +1,20 @@
 package com.example.TestService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
 @SpringBootApplication
 public class TestServiceApplication {
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestServiceApplication.class, args);
 	}
-
+	
+	
 }
--- src/main/java/com/example/TestService/controller/TestController.java
+++ src/main/java/com/example/TestService/controller/TestController.java
@@ -1,13 +1,22 @@
 package com.example.TestService.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.TestService.entitiy.Customer;
+import com.example.TestService.repository.CustomerRepository;
 
 @RestController
 public class TestController {
 	
+	@Autowired
+	CustomerRepository repo;
+	
 	@GetMapping("/test")
-	public String testService() {
-		return "Hello World from swapnil!!";
+	public List<Customer> testService() {
+		return repo.findAll();
 	}
 }
